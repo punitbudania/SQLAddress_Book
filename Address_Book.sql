fd792b9c81f1d3d56bd7e49f7f925e7c1822e072
punitbudania <punitbudania88@gmail.com>
2020-12-18 22:51:37 +0530

    [punit]Added: eidt data of address book table

--- Address_Book.sql
+++ Address_Book.sql
@@ -7,8 +7,8 @@ UC_2
 CREATE TABLE address_book
 (
  id INT unsigned NOT NULL AUTO_INCREMENT,
- firstname VARCHAR(100) NOT NULL,
- lastname VARCHAR(100) NOT NULL,
+ firstName VARCHAR(100) NOT NULL,
+ lastName VARCHAR(100) NOT NULL,
  address VARCHAR(100) NOT NULL,
  city VARCHAR(100) NOT NULL,
  state VARCHAR(100) NOT NULL,
@@ -24,4 +24,5 @@ INSERT INTO address_book (firstName, lastName, address, city, state, zip, phoneN
  ('Arif', 'Khan', 'Vikas colony', 'Jaipur', 'Rajasthan', 337801, 37602830234, 'akhan@gmail.com'),
  ('Harkirat', 'Singh', 'Bilal colony', 'Panji', 'Goa', 327641, 48709042947, 'hsingh@gmail.com');
 
-
+UC_4
+UPDATE address_book SET address = "Will Colony" WHERE firstName = "Arif";
